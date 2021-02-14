078dd69cb50ff51b0d0d9cf9d5242c1a849c40de
hobbes7878 <jon.r.mcclure@gmail.com>
2021-02-14 13:53:59 +0000

    rebuild

--- src/demo/Chart.svelte
+++ src/demo/Chart.svelte
@@ -61,7 +61,10 @@ Follow the notes below! -->
           .map((d) => d.isoNumeric.padStart(3, '0'))
           .includes(feature.id),
       fill: tooltipCountry
-        ? (d) => (client.getCountry(parseInt(d.id).toString()).name === tooltipCountry ? '#bbb' : '#ddd')
+        ? (d) =>
+            client.getCountry(parseInt(d.id).toString()).name === tooltipCountry
+              ? '#bbb'
+              : '#ddd'
         : '#ddd',
     },
     spikes: {
